11f281a312669a9f456dce57eb2df3faef67c266
seanpeng12 <seanpeng12@gmail.com>
2026-08-25 01:07:47 +0800

    Remove unresolved homepage components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,10 @@
   <div class="home-view">
     <HeroSection />
     <ShopSection />
-    <BlogSection />
-    <InformationSection />
   </div>
 </template>
 
 <script setup>
 import HeroSection from '../components/HeroSection.vue';
 import ShopSection from '../components/ShopSection.vue';
-// import BlogSection from '../components/BlogSection.vue';
-// import InformationSection from '../components/InformationSection.vue';
 </script>
